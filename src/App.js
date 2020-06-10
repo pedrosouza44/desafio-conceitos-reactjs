1bc273c3d653814508cc562e120c7b0be8da76fc
Pedro Souza <pedro_souza44@hotmail.com>
2020-06-10 11:07:28 -0300

    Criar metodos listar, adicionar e excluir

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(res => {
+      setRepositories(res.data);
+    })
+  }, []);
+
+
   async function handleAddRepository() {
     // TODO
+    const res = await api.post('/repositories', {
+      title: `Repositorio ${Date.now()}`,
+      url: 'https://github.com/pedrosouza44/VUTTR-front-end',
+      techs: [ 'ReactJS', 'React Native', 'NodeJS', 'AngularJS', 'CSS', 'HTML5' ]
+    });
+
+    const repositorie = res.data;
+
+    setRepositories([...repositories, repositorie]);
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    const res = await api.delete(`/repositories/${id}`);
+
+    const repositorie = repositories.filter(repo => repo.id !== id);
+
+    setRepositories(repositorie);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repo.id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
